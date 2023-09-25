cfac366cee0ead921214ba288ecf994a39accd5d
Matheus <mn909118@gmail.com>
2023-09-25 16:38:04 -0300

    correcao de bug

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,79 +1,67 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { getAuth } from "firebase/auth";
+
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, } from 'react-native';
-import estilos from '../../css/estilocadastro';
-import app from "../../services/firebase"
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
-export default function Cadastro( {navigation} ) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] =  useState('');
+import app from "../../services/firebase";
+import estilos from "../../css/estilocadastro";
 
-    async function createUser() {
+export default function Cadastro({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        const auth = getAuth(app)
-        await createUserWithEmailAndPassword(auth, email, password)
+  async function createUser() {
+    const auth = getAuth(app);
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log("cadastrado com sucesso - ", credentials.user.uid);
+      })
+      .catch((error) => {
+        console.log("ENOENT ERR!", error.code);
 
-        .then(userCredential => {
-            console.log('cadastrado com sucesso - ', credentials.user.uid);
+        if (error.code == "auth/email-already-in-use") {
+          console.log("Esse Email esta em uso");
+        }
+      });
+  }
 
-        })
-	.catch(error => {
-            console.log("ENOENT ERR!", error.code)
+  return (
+    <View style={estilos.containerPrincipal}>
+      <View>
+        <Text style={estilos.title}></Text>
+      </View>
 
-            if (error.code == "auth/email-already-in-use"){
-
-                console.log("Esse Email esta em uso");
-
-            }
-
-        });
-
-}
-
-    return (
-
-        
-        <View style={estilos.containerPrincipal}>
-
-        <View>
-            <Text style={estilos.title}></Text>
-        </View>
-
-        <View style={estilos.area}>
-            <Text style={estilos.titleArea}>Cadastro</Text>
-        </View>
-        <View style={estilos.container}>
-
-            <TextInput
-            style={estilos.caixasTexto}
-            placeholder="email"
-            placeholderTextColor="#313131"
-            value={email}
-            onChangeText={value => setEmail(value)}
-
+      <View style={estilos.area}>
+        <Text style={estilos.titleArea}>Cadastro</Text>
+      </View>
+      <View style={estilos.container}>
+        <TextInput
+          style={estilos.caixasTexto}
+          placeholder="email"
+          placeholderTextColor="#313131"
+          value={email}
+          onChangeText={(value) => setEmail(value)}
         />
         <TextInput
-            style={estilos.caixaSenha}
-            placeholder="senha"
-            placeholderTextColor="#313131"
-            value={password}
-            onChangeText={value => setPassword(value)}
-
+          style={estilos.caixaSenha}
+          placeholder="senha"
+          placeholderTextColor="#313131"
+          value={password}
+          onChangeText={(value) => setPassword(value)}
         />
 
-        <TouchableOpacity
-            style={estilos.Botao} 
-            onPress={createUser}>
-            <Text style={estilos.textoBotao}>Cadastrar-se</Text>
+        <TouchableOpacity style={estilos.Botao} onPress={createUser}>
+          <Text style={estilos.textoBotao}>Cadastrar-se</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity 
-        onPress={() => navigation.navigate('Login')}
-        style={estilos.Botao}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Login")}
+          style={estilos.Botao}
+        >
           <Text style={estilos.textoBotao}>Entrar</Text>
         </TouchableOpacity>
-        </View>
-        </View>
-    )
+      </View>
+    </View>
+  );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,73 +1,74 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ImageBackground, Image} from 'react-native';
-import estilos from '../../css/estilologin';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+  Image,
+} from "react-native";
+
+import estilos from "../../css/estilologin";
+
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from "../../services/firebase";
 
-
-export default function Login( {navigation} ){
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] =  useState('');
-
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   async function singnIn() {
-  const auth = getAuth(app);
+    const auth = getAuth(app);
     await signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => { 
-    
-  {() => navigation.navigate('Home')}
-    console.log('Usuário entrou! - ', userCredential.user);
+      .then((userCredential) => {
+        {
+          () => navigation.navigate("Home");
+        }
+        console.log("Usuário entrou! - ", userCredential.user);
+      })
+      .catch((error) => {
+        console.log("ENOENT ERR!", error.code);
+      });
+  }
 
-  })
-  .catch((error) => {
-    console.log("ENOENT ERR!", error.code)
+  return (
+    <View style={estilos.containerPrincipal}>
+      <ImageBackground
+        style={estilos.imagemFundo}
+        source={require("../../img/Rectangle.jpg")}
+      >
+        <View>
+          <Text style={estilos.title}>Login</Text>
+        </View>
 
-  });
-}
-
-
-return(
-  <View style={estilos.containerPrincipal}>
-
-    <ImageBackground
-      style={estilos.imagemFundo} source={require('../../img/Rectangle.jpg')}>
-      
-
-      <View>
-        <Text style={estilos.title}>Login</Text>
-      </View>
-
-    <Image style={estilos.imagemLogo} source={require('../../img/Logo.png')}/>
+        <Image
+          style={estilos.imagemLogo}
+          source={require("../../img/Logo.png")}
+        />
 
         <TextInput
           style={estilos.caixaEmail}
           value={email}
-          placeholder='  Email'
+          placeholder="  Email"
           onChangeText={(texto) => setEmail(texto)}
         />
-        
-        <TextInput 
+
+        <TextInput
           style={estilos.caixaSenha}
-          placeholder=' Senha'    
-          value={password}   
+          placeholder=" Senha"
+          value={password}
           onChangeText={(password) => setPassword(password)}
-          secureTextEntry   
+          secureTextEntry
         />
-        
-        <TouchableOpacity 
-        onPress={singnIn}
-        style={estilos.Botao}>
+
+        <TouchableOpacity onPress={singnIn} style={estilos.Botao}>
           <Text style={estilos.textoBotao}>Entrar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity 
-        style={estilos.Botao}>
+        <TouchableOpacity style={estilos.Botao}>
           <Text style={estilos.textoBotao}>não tem uma conta?</Text>
         </TouchableOpacity>
-
-
-    </ImageBackground>
-  </View>
-)
+      </ImageBackground>
+    </View>
+  );
 }
